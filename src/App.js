fee480fc8c49b53875234d40b010b65c20d8afea
ikram abdillah <ikram.koli@gmail.com>
2022-09-11 14:11:53 +0700

    Pazar Beta 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Product from "./components/Product";
+import Jumbotron from "./components/Jumbotron";
+import Detail from "./components/Detail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Jumbotron />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/products/:id" element={<Detail />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
-
 export default App;
---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ciwi from "../assets/bg.jpg";
+import Product from "./Product";
+
+function Jumbotron() {
+  return (
+    <div className="hero">
+      <div className="card bg-dark text-black border-0">
+        <img src={ciwi} className="card-img" alt="Background" height="550px" />
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            <h5 className="card-title display-3 fw-bolder mb-0"> NEW SEASON COMING </h5>
+            <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+          </div>
+        </div>
+      </div>
+      <Product />
+    </div>
+  );
+}
+
+export default Jumbotron;
