265f939422ab5e3ece2e32832c81fa596172d02c
Muhamad-Fatah <muhamad.fatah494@gmail.com>
2022-09-12 13:08:29 +0700

    Fix navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,52 +1,94 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import {Button} from "react-bootstrap"
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
+
+  const [login, setLogin] = useState(true)
+
   return (
     <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand className="navbar-brand fw-bold fs-1" href="#">
-          Pazar
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="mx-auto my-2 my-lg-0 w-50 " style={{ maxHeight: "100px" }} navbarScroll>
-            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink className="nav-link active fw-bold fs-5 " aria-current="page" to="/">
-                  Home
-                </NavLink>
-              </li>
+      {login ?
+        <Container>
+          <Navbar.Brand className="navbar-brand fw-bold fs-1" href="#">
+            Pazar
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="mx-auto my-2 my-lg-0 w-50 " style={{ maxHeight: "100px" }} navbarScroll>
+              <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink className="nav-link active fw-bold fs-5 " aria-current="page" to="/">
+                    Home
+                  </NavLink>
+                </li>
 
-              <li className="nav-item">
-                <NavLink className="nav-link active fw-bold fs-5" aria-current="page" to="/products">
-                  Product
-                </NavLink>
-              </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link active fw-bold fs-5" aria-current="page" to="/products">
+                    Product
+                  </NavLink>
+                </li>
 
-              <Form className="d-flex w-75">
-                <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
-              </Form>
-            </ul>
-          </Nav>
-          <Form className="buttons">
-            <NavLink to="/login" className="btn btn-outline-dark me-2">
-              <i className="fa fa-sign-in  "></i> Login
-            </NavLink>
+                <Form className="d-flex w-75">
+                  <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+                </Form>
+              </ul>
+            </Nav>
+            <Form className="buttons">
+              <NavLink to="/cart" className="btn btn-outline-dark me-2">
+                <i className="fa fa-shopping-cart me-1"></i> Cart(0)
+              </NavLink>
+              <NavLink to="/profile" className="btn btn-outline-dark me-2">
+                <i className="fa fa-user me-1"></i> Profile
+              </NavLink>
+              <Button onClick={() => setLogin(prev => !prev)}>TEST</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container> :
+        <Container>
+          <Navbar.Brand className="navbar-brand fw-bold fs-1" href="#">
+            Pazar
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="mx-auto my-2 my-lg-0 w-50 " style={{ maxHeight: "100px" }} navbarScroll>
+              <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink className="nav-link active fw-bold fs-5 " aria-current="page" to="/">
+                    Home
+                  </NavLink>
+                </li>
 
-            <NavLink to="/register" className="btn btn-outline-dark me-2">
-              <i className="fa fa-user-plus  me-1"></i> Register
-            </NavLink>
+                <li className="nav-item">
+                  <NavLink className="nav-link active fw-bold fs-5" aria-current="page" to="/products">
+                    Product
+                  </NavLink>
+                </li>
 
-            {/* <NavLink to="/cart" className="btn btn-outline-dark me-2">
-              <i className="fa fa-shopping-cart me-1"></i> Cart(0)
-            </NavLink> */}
-          </Form>
-        </Navbar.Collapse>
-      </Container>
+                <Form className="d-flex w-75">
+                  <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+                </Form>
+              </ul>
+            </Nav>
+            <Form className="buttons">
+              <NavLink to="/login" className="btn btn-outline-dark me-2">
+                <i className="fa fa-sign-in  "></i> Login
+              </NavLink>
+
+              <NavLink to="/register" className="btn btn-outline-dark me-2">
+                <i className="fa fa-user-plus  me-1"></i> Register
+              </NavLink>
+
+              <Button onClick={() => setLogin(prev => !prev)}>TEST</Button>
+
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      }
     </Navbar>
   );
 }
