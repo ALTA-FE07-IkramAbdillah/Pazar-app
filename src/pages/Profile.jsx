67dfcad80bc10d02ce3dd54cfa75b2a40c743189
evialf <vifia.rales@gmail.com>
2022-09-14 16:53:28 +0700

    change profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Modal, NavLink, Row } from "react-bootstrap";
+import { Button, Col, Container, NavLink, Row } from "react-bootstrap";
 import "../App.css";
 
 const Profile = () => {
@@ -10,7 +10,7 @@ const Profile = () => {
           <Col>
             <h6 className="fs-5">userName</h6>
             <i className="fa fa-reguler fa-user fa-4x"></i>
-            <a className="text-decoration-none" href="#">
+            <a className="text-decoration-none mx-2" href="#">
               Edit Profile
             </a>
             <hr />
@@ -28,54 +28,83 @@ const Profile = () => {
 
         <Row>
           <Col xs={4}>
-            <Row className="px-2 py-3">
-              <h6>My profile</h6>
-              <h6>
-                <i className="fa fa-light fa-user"></i>
-                <a className="text-decoration-none" href="#">
-                  Address
-                </a>
-              </h6>
+            <Row className="mb-4">
+              <Col className="text-end">
+                <i className="fa fa-light fa-user fa-2x"></i>
+              </Col>
+              <Col className="text-start">
+                <h6 className="">My profile</h6>
+                <h6>
+                  <a className="text-decoration-none" href="#">
+                    Address
+                  </a>
+                </h6>
+              </Col>
             </Row>
-            <Row className="px-2 py-3">
-              <h6>
-                <i class="fa-solid fa-list-radio"></i>
-                My order
-              </h6>
+
+            <Row className="mb-4">
+              <Col className="text-end">
+                <i className="fa fa-user fa-list-radio fa-2x"></i>
+              </Col>
+              <Col className="text-start">
+                <h6>
+                  <a className="text-decoration-none text-dark" href="#">
+                    My Order
+                  </a>
+                </h6>
+              </Col>
             </Row>
-            {/* <Modal.Dialog className="px-2 py-3"> */}
-            <Modal.Body>
-              <h6 className="text-danger">
-                <i class="fa fa-user-xmark"></i>
-                Delete Account
-              </h6>
-            </Modal.Body>
-            {/* </Modal.Dialog> */}
+
+            <Row className="mb-4">
+              <Col className="text-end">
+                <i className="fa fa-user fa-xmark fa-2x"></i>
+              </Col>
+              <Col className="text-start">
+                <NavLink>
+                  <h6>
+                    <a className="text-decoration-none text-danger" href="#">
+                      Delete Account
+                    </a>
+                  </h6>
+                </NavLink>
+              </Col>
+            </Row>
           </Col>
 
-          <Col xs={4}>
-            <h6 className="text-secondary">Username</h6>
-
+          <Col xs={2} className="text-secondary text-end">
+            <h6 className="mb-3">Username</h6>
             <form>
               <label>
-                <h6>
-                  Nama
-                  <input type="text" nama="nama" />
-                </h6>
+                <h6 className="mb-4">Nama</h6>
               </label>
             </form>
-            <h6 className="text-secondary">Email</h6>
-            <h6 className="text-secondary">Nomor Telepon</h6>
+            <h6 className="mb-3">Email</h6>
+            <h6 className="mb-3">Nomor Telepon</h6>
+          </Col>
+
+          <Col xs={2} className="text-start">
+            <h6 className="mb-3">UserName</h6>
+            <form>
+              <label>
+                <input className="mb-3" type="text" nama="nama" />
+              </label>
+            </form>
+            <h6 className="mb-3">email</h6>
+            <h6 className="mb-3">nomorTelp</h6>
           </Col>
 
           <Col xs={4}>
             <div className="vl">
-              <div className="me-4">
+              <div className="m-3">
                 <i class="fa fa-user fa-5x"></i>
               </div>
-              <NavLink to="/" className="btn btn-outline-white border-dark">
+              <Button
+                to="/"
+                variant="light"
+                className="btn btn-outline-white border-dark mx-3"
+              >
                 <i className="fa fa-image"></i> Browse Image
-              </NavLink>
+              </Button>
             </div>
           </Col>
         </Row>
